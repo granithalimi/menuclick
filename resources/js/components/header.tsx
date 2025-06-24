b8e1fb67f2f1d13f55d8efdb1dc49c281f340b0c
Granit Halimi <granithalimi@Granits-MacBook-Air.local>
2025-06-24 20:00:11 +0200

    designed the header

--- resources/js/components/header.tsx
+++ resources/js/components/header.tsx
@@ -1,7 +1,21 @@
+import { Link, usePage } from "@inertiajs/react";
+
 export default function Header(){
+    const { url } = usePage();
     return (
-        <div className="text-black w-full bg-blue-100 h-20">
-            Header
+        <div className="text-black w-full bg-gray-800 h-20">
+            <div className="w-2/3 mx-auto h-full  flex justify-between items-center">
+            <div className="w-20 h-20 flex justify-center items-center">
+                <img src={`${window.location.origin}/storage/images/logo.png`} alt="" className="w-12" />
+            </div>
+            <div className="flex gap-3">
+                <Link href={route("dashboard")} className={`${url === "/dashboard" ? "text-white font-extrabold" : "text-gray-500"}  hover:text-white duration-300`}>Dashboard</Link>
+                <Link href={route("table.index")} className={`${url === "/table" ? "text-white font-extrabold " : "text-gray-500"} hover:text-white duration-300`}>Tables</Link>
+                <Link href={route("category.index")} className={`${url === "/category" ? "text-white font-extrabold" : "text-gray-500"} hover:text-white duration-300`}>Categories</Link>
+                <Link href={route("product.index")} className={`${url === "/product" ? "text-white font-extrabold" : "text-gray-500"} hover:text-white duration-300`}>Products</Link>
+                <Link href={route("order.index")} className={`${url === "/order" ? "text-white font-extrabold" : "text-gray-500"} hover:text-white duration-300`}>Orders</Link>
+            </div>
+            </div>
         </div>
     )
 }
